9998c3b7cf19cc9641c9785d2514cd5dde0d7d6c
ramonsamagaio <70339182+ramonsamagaio@users.noreply.github.com>
2026-08-28 07:01:07 -0300

    Use adaptive RIFTWORKS HUD by default in Unreal

--- Unreal/RiftworksUE/Source/RiftworksUE/RiftGameMode.cpp
+++ Unreal/RiftworksUE/Source/RiftworksUE/RiftGameMode.cpp
@@ -1,7 +1,9 @@
 #include "RiftGameMode.h"
+#include "RiftHUD.h"
 #include "RiftPlayerCharacter.h"
 
 ARiftGameMode::ARiftGameMode()
 {
     DefaultPawnClass = ARiftPlayerCharacter::StaticClass();
+    HUDClass = ARiftHUD::StaticClass();
 }
