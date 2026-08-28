5d53185e8e5cf7b49e81d4a5350741f42303ce76
ramonsamagaio <70339182+ramonsamagaio@users.noreply.github.com>
2026-08-28 06:44:33 -0300

    Add Blueprintable RIFTWORKS game mode

---
+++ Unreal/RiftworksUE/Source/RiftworksUE/RiftGameMode.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "RiftGameMode.generated.h"
+
+UCLASS(Blueprintable)
+class RIFTWORKSUE_API ARiftGameMode : public AGameModeBase
+{
+    GENERATED_BODY()
+
+public:
+    ARiftGameMode();
+};
