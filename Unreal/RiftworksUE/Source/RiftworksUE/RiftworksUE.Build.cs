8b12fd11e8e4c7f48e48fd0e139f3c9f0963e62d
ramonsamagaio <70339182+ramonsamagaio@users.noreply.github.com>
2026-08-28 07:29:15 -0300

    Remove unused EnhancedInput dependency from native runtime

--- Unreal/RiftworksUE/Source/RiftworksUE/RiftworksUE.Build.cs
+++ Unreal/RiftworksUE/Source/RiftworksUE/RiftworksUE.Build.cs
@@ -13,7 +13,6 @@ public class RiftworksUE : ModuleRules
             "CoreUObject",
             "Engine",
             "InputCore",
-            "EnhancedInput",
             "AIModule",
             "NavigationSystem",
             "GameplayTasks",
