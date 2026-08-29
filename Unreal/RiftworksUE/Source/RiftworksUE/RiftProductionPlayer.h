79c0931c8327cb42d767bbdd416912596b8de1f9
ramonsamagaio <70339182+ramonsamagaio@users.noreply.github.com>
2026-08-28 21:36:30 -0300

    Add player-facing FAS connection tool

--- Unreal/RiftworksUE/Source/RiftworksUE/RiftProductionPlayer.h
+++ Unreal/RiftworksUE/Source/RiftworksUE/RiftProductionPlayer.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "RiftEngineeringJoint.h"
 #include "RiftPlayerCharacter.h"
 #include "RiftProductionPlayer.generated.h"
 
@@ -12,6 +13,7 @@ class RIFTWORKSUE_API ARiftProductionPlayerCharacter : public ARiftPlayerCharact
 public:
     ARiftProductionPlayerCharacter();
     virtual void Tick(float DeltaSeconds) override;
+    virtual void SetupPlayerInputComponent(UInputComponent* PlayerInputComponent) override;
 
     UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="RIFTWORKS|Movement|Stamina")
     float MaxStamina = 100.0f;
@@ -46,8 +48,32 @@ public:
     UPROPERTY(BlueprintReadOnly, Category="RIFTWORKS|Movement|Stamina")
     bool bSprintExhausted = false;
 
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="RIFTWORKS|Engineering")
+    ERiftJointMode SelectedJointMode = ERiftJointMode::Weld;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="RIFTWORKS|Engineering")
+    float EngineeringTraceDistance = 1350.0f;
+
+    UPROPERTY(BlueprintReadOnly, Category="RIFTWORKS|Engineering")
+    TObjectPtr<ARiftAssemblyPart> EngineeringSelectionA;
+
+    UPROPERTY(BlueprintReadOnly, Category="RIFTWORKS|Engineering")
+    TObjectPtr<ARiftEngineeringJoint> LastCreatedJoint;
+
+    UFUNCTION(BlueprintCallable, Category="RIFTWORKS|Engineering")
+    void CycleEngineeringJointMode(int32 Direction = 1);
+
+    UFUNCTION(BlueprintCallable, Category="RIFTWORKS|Engineering")
+    void CancelEngineeringSelection();
+
     UFUNCTION(BlueprintImplementableEvent, Category="RIFTWORKS|Blueprint Events")
     void BP_OnStaminaChanged(float NewStamina, float MaximumStamina, bool bExhausted);
+
+    UFUNCTION(BlueprintImplementableEvent, Category="RIFTWORKS|Blueprint Events")
+    void BP_OnEngineeringSelectionChanged(ARiftAssemblyPart* FirstPart, ERiftJointMode JointMode);
+
+    UFUNCTION(BlueprintImplementableEvent, Category="RIFTWORKS|Blueprint Events")
+    void BP_OnEngineeringJointCreated(ARiftEngineeringJoint* Joint);
 
 protected:
     virtual void BeginPlay() override;
@@ -58,4 +84,10 @@ protected:
 
     void UpdateStamina(float DeltaSeconds);
     void UpdateFirstPersonCameraMotion(float DeltaSeconds);
+    void EngineeringConnectPressed();
+    void EngineeringModeNextPressed();
+    void EngineeringCancelPressed();
+    ARiftAssemblyPart* TraceEngineeringPart(FHitResult* OutHit = nullptr) const;
+    FString EngineeringModeName() const;
+    void UpdateEngineeringPrompt();
 };
