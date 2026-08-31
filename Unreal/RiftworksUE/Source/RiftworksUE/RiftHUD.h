43349a43914be9ee23d7af9bb934273182ea9d59
ramonsamagaio <70339182+ramonsamagaio@users.noreply.github.com>
2026-08-31 16:19:26 -0300

    Add visual drag and drop inventory HUD

--- Unreal/RiftworksUE/Source/RiftworksUE/RiftHUD.h
+++ Unreal/RiftworksUE/Source/RiftworksUE/RiftHUD.h
@@ -4,6 +4,9 @@
 #include "GameFramework/HUD.h"
 #include "RiftHUD.generated.h"
 
+class ARiftLootContainer;
+class ARiftPlayerCharacter;
+
 UCLASS(Blueprintable)
 class RIFTWORKSUE_API ARiftHUD : public AHUD
 {
@@ -11,6 +14,8 @@ class RIFTWORKSUE_API ARiftHUD : public AHUD
 
 public:
     virtual void DrawHUD() override;
+    virtual void NotifyHitBoxClick(FName BoxName) override;
+    virtual void NotifyHitBoxRelease(FName BoxName) override;
 
     UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="RIFTWORKS|HUD")
     bool bDrawNativeFallback = true;
@@ -18,6 +23,36 @@ public:
     UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="RIFTWORKS|HUD")
     float SafeMargin = 24.0f;
 
+    UPROPERTY(BlueprintReadOnly, Category="RIFTWORKS|Inventory")
+    bool bInventoryVisible = false;
+
+    UPROPERTY(BlueprintReadOnly, Category="RIFTWORKS|Inventory")
+    TObjectPtr<ARiftLootContainer> OpenContainerActor;
+
+    UFUNCTION(BlueprintCallable, Category="RIFTWORKS|Inventory")
+    void ToggleInventory();
+
+    UFUNCTION(BlueprintCallable, Category="RIFTWORKS|Inventory")
+    void OpenContainer(ARiftLootContainer* Container);
+
+    UFUNCTION(BlueprintCallable, Category="RIFTWORKS|Inventory")
+    void CloseInventory();
+
     UFUNCTION(BlueprintImplementableEvent, Category="RIFTWORKS|Blueprint Events")
     void BP_DrawRiftHUD(float ScreenWidth, float ScreenHeight);
+
+private:
+    void DrawStatusHUD(ARiftPlayerCharacter* Player, float W, float H);
+    void DrawInventoryHUD(ARiftPlayerCharacter* Player, float W, float H);
+    void RefreshPlayerSlotOrder(ARiftPlayerCharacter* Player);
+    void SetInventoryInputMode(bool bEnabled);
+    int32 GetPlayerItemCount(const ARiftPlayerCharacter* Player, FName ItemId) const;
+    bool TransferPlayerToContainer(ARiftPlayerCharacter* Player, int32 PlayerSlot, int32 ContainerSlot);
+    bool TransferContainerToPlayer(ARiftPlayerCharacter* Player, int32 ContainerSlot, int32 PlayerSlot);
+    bool ParseSlotName(FName BoxName, TCHAR& OutSide, int32& OutIndex) const;
+    FLinearColor ItemColor(FName ItemId) const;
+
+    TArray<FName> PlayerSlotOrder;
+    FName DragSourceBox = NAME_None;
+    FText InventoryMessage;
 };
